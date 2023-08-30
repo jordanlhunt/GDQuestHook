76a899d7fb10167557b528543faa1607d181611e
Jordan "Heroic" Hunt <jordan.hunt@g.austincc.edu>
2023-08-29 21:57:06 -0500

    Completed State.gd

--- src/Main/StateMachine/State.gd
+++ src/Main/StateMachine/State.gd
@@ -1,4 +1,33 @@
 extends Node
-
-
 class_name State, "res://assets/icons/state.svg"
+"""
+An interface to use in the Hierarchical State Machine
+The lowest leaf tries to handle callbacks. If it cannot it delegates the work to its parent.
+Use State as a child of a StateMachine node.
+"""
+
+# Reference to the StateMachine it belongs to
+onready var parentStateMachine = GetStateMachine(self)
+
+
+# A recursive call to get the StateMachine node a state belongs to
+func GetStateMachine(node: Node) -> Node:
+	if node != null and not node.is_in_group("StateMachineGroup"):
+		return GetStateMachine(node.get_parent())
+	return node
+
+
+func UnhandledInput(event: InputEvent) -> void:
+	return
+
+
+func PhysicsProcess(delta: float) -> void:
+	return
+
+
+func TransitionState(message: Dictionary = {}) -> void:
+	return
+
+
+func ExitState() -> void:
+	return
--- src/Main/StateMachine/StateMachine.gd
+++ src/Main/StateMachine/StateMachine.gd
@@ -1,4 +1,7 @@
 extends Node
 
-
 class_name StateMachine, "res://assets/icons/state_machine.svg"
+
+"""
+Generic State Machine. It creates States and delegates engine callbacks (_physics_process, _unhandled_input) to current active state
+"""
